cbf26d4ff7686b24d2d450850fddcf0f5edcf351
indrajith <indrajith.koloorkavil@gmail.com>
2020-12-21 18:43:46 +0530

    EZ-762: Language switcher

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
-(function(w) {
+(function($) {
   var sw = document.body.clientWidth,
     sh = document.body.clientHeight;
 
-  $(w).resize(function() {
+  $(document).resize(function() {
     //Update dimensions on resize
     sw = document.body.clientWidth;
     sh = document.body.clientHeight;
@@ -23,4 +23,72 @@
     $(this).toggleClass('active');
     $('.header .search-form').toggleClass('active');
   });
-})(this);
+
+  var languageswitcher = function(){
+    jQuery('#block-languageswitcher ul.links').each(function() {
+      var select = jQuery(document.createElement('select')).insertBefore(jQuery(this).hide());
+      jQuery('>li a', this).each(function() {
+        var a = jQuery(this).click(function() {
+          if (jQuery(this).attr('target')==='_blank') {
+            window.open(this.href);
+          }
+          else {
+            window.location.href = this.href;
+          }
+        }),
+        option = jQuery(document.createElement('option')).appendTo(select).val(this.href).html(jQuery(this).html()).click(function() {
+          a.click();
+        });
+      });
+    });
+    
+    var li_index = jQuery('#block-languageswitcher ul li.is-active').index();
+    jQuery('#block-languageswitcher select option').eq(li_index).attr("selected", "selected");
+  
+    jQuery('#block-languageswitcher select').change(function(){
+      window.location.href = jQuery(this).val();   
+    });
+  
+    
+  }
+  languageswitcher();
+  
+
+})(jQuery);
+
+//language switcher
+// var languageswitcher = function(){
+//   jQuery('#block-languageswitcher ul.links').each(function() {
+//     var select = jQuery(document.createElement('select')).insertBefore(jQuery(this).hide());
+//     jQuery('>li a', this).each(function() {
+//       var a = jQuery(this).click(function() {
+//         if (jQuery(this).attr('target')==='_blank') {
+//           window.open(this.href);
+//         }
+//         else {
+//           window.location.href = this.href;
+//         }
+//       }),
+//       option = jQuery(document.createElement('option')).appendTo(select).val(this.href).html(jQuery(this).html()).click(function() {
+//         a.click();
+//       });
+//     });
+//   });
+
+//   var li_index = jQuery('#block-languageswitcher ul li.is-active').index();
+//   jQuery('#block-languageswitcher select option').eq(li_index).attr("selected", "selected");
+
+//   jQuery('#block-languageswitcher select').change(function(){
+//     window.location.href = jQuery(this).val();   
+//   });
+
+  
+// }
+
+// jQuery(document).ready(function() {
+//   languageswitcher(); 
+// });
+
+// jQuery(document).load(function(){
+//   languageswitcher();
+// })

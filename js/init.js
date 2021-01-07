35eab126fa0f99c983352bc808c831985191e8d4
indrajith <indrajith.koloorkavil@gmail.com>
2021-01-07 17:45:22 +0530

    EZ-762: Language switcher PR updated

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
-(function(w) {
+(function($) {
   var sw = document.body.clientWidth,
     sh = document.body.clientHeight;
 
-  $(w).resize(function() {
+  $(document).resize(function() {
     //Update dimensions on resize
     sw = document.body.clientWidth;
     sh = document.body.clientHeight;
@@ -23,4 +23,34 @@
     $(this).toggleClass('active');
     $('.header .search-form').toggleClass('active');
   });
-})(this);
+
+  //language switcher
+  var languageswitcher = function(){
+    $('#block-languageswitcher ul.links').each(function() {
+      var select = $(document.createElement('select')).insertBefore($(this).hide());
+      $('>li a', this).each(function() {
+        var a = $(this).click(function() {
+          if ($(this).attr('target')==='_blank') {
+            window.open(this.href);
+          }
+          else {
+            window.location.href = this.href;
+          }
+        }),
+        option = $(document.createElement('option')).appendTo(select).val(this.href).html($(this).html()).click(function() {
+          a.click();
+        });
+      });
+    });
+    
+    var li_index = $('#block-languageswitcher ul li.is-active').index();
+    $('#block-languageswitcher select option').eq(li_index).attr("selected", "selected");
+  
+    $('#block-languageswitcher select').change(function(){
+      window.location.href = $(this).val();   
+    });
+    
+  }
+  languageswitcher();
+  
+})(jQuery);

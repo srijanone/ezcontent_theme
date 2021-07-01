7e0ef1f33f922211d34f2708aa4397819022a150
harish kumar <harish.kumar@srijan.net>
2021-07-01 14:28:23 +0530

    Ez-959-landing page edit media button hide issue

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+//layout configration block -- media edit block box height issue
+jQuery(document).ajaxComplete(function(){
+  jQuery(".ui-dialog  .ui-widget-content.ui-dialog-buttonpane.ui-helper-clearfix").siblings(".ui-dialog .ui-widget-content.ui-dialog-content").addClass("edit-ui-widget");
+});

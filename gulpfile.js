96739f81de71919aa5df59d5a445e382c1b0a90f
Saurav Kumar <saurav.kumar@materialplus.io>
2023-12-12 16:12:09 +0530

    style changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,86 +2,79 @@
 var gulp = require('gulp');
 var argv = require('yargs').argv;
 var sassLint = require('gulp-sass-lint');
-
 var autoprefixer = require('gulp-autoprefixer');
 var babel = require('gulp-babel');
 var bs = require('browser-sync').create();
 var pixrem = require('gulp-pixrem');
-var sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 var sassGlob = require('gulp-sass-glob');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var browserslist = require('browserslist');
 
 var paths = {
-	//sassSrc: './scss/**/**/*.{scss,sass}',
-	sassSrc: './pattern-lab/source/_patterns/**/**/*.{scss,sass}',
-	sassDest: 'css',
-	jsSrc: ['js/source/*.js', 'js/component/*.js'],
-	jsDest: 'js/build',
-	imgSrc: 'images/source/**/*.{png,jpg,gif}',
-	imgDest: 'images/optimized',
-	svgSrc: 'images/source/**/*.svg',
-	svgDest: 'images/optimized'
-}
-
-var browserList = ['last 5 versions', '> 5%', 'Firefox ESR'];
+  sassSrc: './pattern-lab/source/_patterns/**/**/*.{scss,sass}',
+  sassDest: 'css',
+  jsSrc: ['js/source/*.js', 'js/component/*.js'],
+  jsDest: 'js/build',
+  imgSrc: 'images/source/**/*.{png,jpg,gif}',
+  imgDest: 'images/optimized',
+  svgSrc: 'images/source/**/*.svg',
+  svgDest: 'images/optimized'
+};
 
 gulp.task('scss', function () {
-	return gulp.src(paths.sassSrc)
-		.pipe(sourcemaps.init())
-		.pipe(sassGlob())
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // use for dev (expanded)
-		// Need the next two lines as an intermediate write, otherwise autoprefizer doesnt cooperate with sourcemaps
-		// https://github.com/ByScripts/gulp-sample/blob/master/gulpfile.js
-		.pipe(sourcemaps.write({includeContent: false}))
-    .pipe(sourcemaps.init({loadMaps: true}))
-		.pipe(autoprefixer({browsers: browserList}))
-		.pipe(pixrem())
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(paths.sassDest))
-		.pipe(bs.stream());
-});
-
-gulp.task('js', function() {
-	return gulp.src(paths.jsSrc)
-		.pipe(sourcemaps.init())
-		.pipe(babel({
-			presets: [
-				['env', {
-					targets: {
-						browsers: browserList
-					}
-				}]
-			]
-		}))
-		.pipe(uglify())
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest(paths.jsDest));
-});
-
-gulp.task('sass-lint', function() {
   return gulp.src(paths.sassSrc)
-	.pipe(sassLint({
-		configFile: '.sasslintrc'
-	}))
-	.pipe(sassLint.format())
-	.pipe(sassLint.failOnError())
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sourcemaps.write({ includeContent: false }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(autoprefixer())
+    .pipe(pixrem())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.sassDest))
+    .pipe(bs.stream());
+});
+
+gulp.task('js', function () {
+  return gulp.src(paths.jsSrc)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: [
+        ['@babel/preset-env', {
+          targets: browserslist()
+        }]
+      ]
+    }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.jsDest));
+});
+
+gulp.task('sass-lint', function () {
+  return gulp.src(paths.sassSrc)
+    .pipe(sassLint({
+      configFile: '.sasslintrc'
+    }))
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
 });
 
 // Watch files for change and set Browser Sync
-gulp.task('watch', function() {
-	bs.init({
-		files: [
-			'css/**/*.css',
-			'templates/**/*.twig',
-			'images/optimized/**/*.{png,jpg,gif,svg}',
-			'js/build/**/*.js',
-			'*.theme'
-		],
-		proxy: argv.proxy
-	});
-	gulp.watch(paths.sassSrc, ['scss']);
-	gulp.watch(paths.jsSrc, ['js']).on('change', bs.reload);
+gulp.task('watch', function () {
+  bs.init({
+    files: [
+      'css/**/*.css',
+      'templates/**/*.twig',
+      'images/optimized/**/*.{png,jpg,gif,svg}',
+      'js/build/**/*.js',
+      '*.theme'
+    ],
+    proxy: argv.proxy
+  });
+  gulp.watch(paths.sassSrc, gulp.series('scss'));
+  gulp.watch(paths.jsSrc, gulp.series('js')).on('change', bs.reload);
 });
 
 // Default task
@@ -89,4 +82,3 @@ gulp.task('default', gulp.series('scss', 'js', 'sass-lint', 'watch'));
 
 // Build Prod
 gulp.task('build:prod', gulp.series('scss', 'js', 'sass-lint'));
-
